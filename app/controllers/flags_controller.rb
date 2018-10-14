71650ebe06179127570f74492b79609a706c2bc7
Matias Crizul <crizulm@gmail.com>
2018-10-14 18:29:10 -0300

    List Flags and change status

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,8 +1,12 @@
 class FlagsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :index]
 
   def new
     @flag = Flag.new
+  end
+
+  def index
+   @flags = Flag.where(organization_id: current_user.organization_id).includes(:organization)
   end
 
   def create
@@ -12,6 +16,15 @@ class FlagsController < ApplicationController
     @report = Report.new(total_request: 0, true_answer: 0, false_answer: 0, total_time: 0)
     @flag.report = @report
     @flag.save
+  end
+
+  def update
+    @flag = Flag.find(params[:id])
+
+    @flag.active = !@flag.active
+    @flag.save
+
+    redirect_to flags_path
   end
 
   private
