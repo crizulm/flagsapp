e19d5606d96a8079124e013016c5969692e38b40
Matias Crizul <crizulm@gmail.com>
2018-10-15 00:44:24 -0300

    Limit home flags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @flags = Flag.where(organization_id: current_user.organization_id).includes(:organization)
+    @flags = Flag.where(organization_id: current_user.organization_id).limit(5)
   end
 
 end
