d3a1333a5d30341c1a749f02a31d23f96d8715c2
Matias Crizul <crizulm@gmail.com>
2018-10-15 00:40:04 -0300

    Refactor new flag form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :flags
+  get 'home/index'
 
-  root to: "flags#index"
+  root to: "home#index"
 end
