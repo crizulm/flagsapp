3512a807e59253473b68748cf03c4b6ca84d0cc9
Matias Crizul <crizulm@gmail.com>
2018-11-24 20:34:33 -0300

    Delete Model, and Mailer Invites

--- app/mailers/invite_mailer.rb
+++
@@ -1,7 +0,0 @@
-class InviteMailer < ApplicationMailer
-  def new_user_invite(invite, url)
-    @invite = invite
-    @url = url
-    mail(to: invite.email, subject: 'You are invited to join - Flagsapp')
-  end
-end
