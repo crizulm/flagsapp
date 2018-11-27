9d13c0424896642ea03898814f203cf15f844bf0
Matias Crizul <crizulm@gmail.com>
2018-11-27 17:02:50 -0300

    Fix error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def authenticate_admin!
-    (current_user == nil) ? redirect_to home_url : redirect_to home_url unless current_user.is_admin
+    if current_user == nil
+      redirect_to home_url
+    else
+      redirect_to home_url unless current_user.is_admin
+    end
   end
 end
